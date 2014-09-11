49101d206f55c2150556f6c0a09f74f8a06a483e
larko <larkox@gmail.com>
2014-09-11 09:09:27 +0100

    Improved the style of the map loading

--- dad.duckkogs.map.js
+++ dad.duckkogs.map.js
@@ -6,59 +6,63 @@ function loadMap(file) {
     xmlhttp.open("GET",file,true);
     xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState == 3)
-            loadMapTextures()
+            loadMapTextures(result, xmlhttp.responseText);
     }
     xmlhttp.send();
     return result;
-    function loadMapTextures() {
-        var ready_textures, game_map;
-        game_map = JSON.parse(xmlhttp.responseText);
-        ready_textures = 0;
-        game_map.textures_images = [];
-        for (var texture = 0; texture < game_map.textures.length; texture++) {
-            ready_textures[texture] = false;
-            game_map.textures_images[texture] = new Image();
-            game_map.textures_images[texture].onload = function () {
-                ready_textures += 1;
-            }
-            game_map.textures_images[texture].src = game_map.textures[texture];
-        }
-        function checkTextures() {
-            if (ready_textures < game_map.textures.length) {
-                setTimeout(checkTextures, 100);
-            }
-            else {
-                var contexts = [];
-                game_map.images = [];
-                for (var texture = 0; texture < game_map.textures.length; texture++) {
-                    game_map.images[texture] = document.createElement("canvas");
-                    game_map.images[texture].width = constants.SQUAREDIM * game_map.width;
-                    game_map.images[texture].height = constants.SQUAREDIM * game_map.height;
-                    game_map.images[texture].hidden = true;
-                    game_map.images[texture].id = "texture" + texture;
-                    contexts[texture] = game_map.images[texture].getContext("2d");
-                }
-                var texture_pos_x, texture_pos_y;
-                for (var row = 0; row < game_map.height; row++) {
-                    for (var col = 0; col < game_map.width; col++) {
-                        cell = game_map.cells[row][col];
-                        texture_pos_x = (cell.up + cell.down * 2) * constants.SQUAREDIM;
-                        texture_pos_y = (cell.left + cell.right * 2) * constants.SQUAREDIM;
-                        cell.pos = {"x": col, "y": row};
-                        cell.occupied = false;
-                        cell.occupied_by = null;
-                        cell.has_exit = texture_pos_x + texture_pos_y != 0;
-                        for (var texture = 0; texture < game_map.textures.length; texture++) {
-                            contexts[texture].drawImage(game_map.textures_images[texture],
-                                    texture_pos_x, texture_pos_y, constants.SQUAREDIM, constants.SQUAREDIM,
-                                    col * constants.SQUAREDIM, row * constants.SQUAREDIM, constants.SQUAREDIM, constants.SQUAREDIM);
-                        }
-                    }
-                }
-                result.map = game_map;
-                result.ready = true;
-            }
-        };
-        checkTextures();
-    }
 }
+
+function loadMapTextures(result, responseText) {
+    var ready_textures, game_map;
+    game_map = JSON.parse(responseText);
+    ready_textures = {"n":0};
+    game_map.textures_images = [];
+    ready_textures = 0;
+    for (var texture = 0; texture < game_map.textures.length; texture++) {
+        game_map.textures_images[texture] = new Image();
+        game_map.textures_images[texture].onload = function () {
+            ready_textures.n += 1;
+        }
+        game_map.textures_images[texture].src = game_map.textures[texture];
+    }
+    checkTextures(result, game_map, ready_textures);
+}
+
+function checkTextures(result, game_map, ready_textures) {
+    if (ready_textures.n < game_map.textures.length) {
+        setTimeout(function() {
+            checkTextures(result, game_map);
+        }, 100);
+    }
+    else {
+        var contexts = [];
+        game_map.images = [];
+        for (var texture = 0; texture < game_map.textures.length; texture++) {
+            game_map.images[texture] = document.createElement("canvas");
+            game_map.images[texture].width = constants.SQUAREDIM * game_map.width;
+            game_map.images[texture].height = constants.SQUAREDIM * game_map.height;
+            game_map.images[texture].hidden = true;
+            game_map.images[texture].id = "texture" + texture;
+            contexts[texture] = game_map.images[texture].getContext("2d");
+        }
+        var texture_pos_x, texture_pos_y;
+        for (var row = 0; row < game_map.height; row++) {
+            for (var col = 0; col < game_map.width; col++) {
+                cell = game_map.cells[row][col];
+                texture_pos_x = (cell.up + cell.down * 2) * constants.SQUAREDIM;
+                texture_pos_y = (cell.left + cell.right * 2) * constants.SQUAREDIM;
+                cell.pos = {"x": col, "y": row};
+                cell.occupied = false;
+                cell.occupied_by = null;
+                cell.has_exit = texture_pos_x + texture_pos_y != 0;
+                for (var texture = 0; texture < game_map.textures.length; texture++) {
+                    contexts[texture].drawImage(game_map.textures_images[texture],
+                            texture_pos_x, texture_pos_y, constants.SQUAREDIM, constants.SQUAREDIM,
+                            col * constants.SQUAREDIM, row * constants.SQUAREDIM, constants.SQUAREDIM, constants.SQUAREDIM);
+                }
+            }
+        }
+        result.map = game_map;
+        result.ready = true;
+    }
+};
